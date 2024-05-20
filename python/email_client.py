087b782c9ea037364aee95ed3d006569bc42b678
Thaarugeshwari <121503925+thaaru05@users.noreply.github.com>
2024-05-20 23:22:03 +0530

    Delete python directory

--- python/email_client.py
+++
@@ -1,8 +0,0 @@
-import socket
-client_socket=socket.socket()
-client_socket.connect(('localhost',587))
-name=input('Enter the data:')
-data=name.encode('utf-8')
-client_socket.send(data)
-received_data=client_socket.recv(1024).decode('utf-8')
-print(received_data)
